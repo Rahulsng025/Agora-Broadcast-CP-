983ee62a69301cb4d493047552c5d4ba25936a40
rahulsng025 <rahul.sng025@gmail.com>
2022-03-24 20:02:40 +0530

    Added meeting  link

--- src/app/pages/meeting-preview/meeting-preview.component.ts
+++ src/app/pages/meeting-preview/meeting-preview.component.ts
@@ -80,7 +80,8 @@ export class MeetingPreviewComponent implements OnInit, OnDestroy {
     const { channel, link } = this.connectionInfoForm?.value;
     if (channel) {
       const joinLink = this.tokeService.getLink(channel);
-      let meetinglink = location.origin+"/#/meeting?channel="+ joinLink;
+      // let meetinglink = location.origin+"/#/meeting?link="+ joinLink;
+      let meetinglink = location.origin+"/#/meeting?channel="+ channel;
       localStorage.setItem('meeting_link',joinLink);
       const formData =new FormData;
       formData.append('live_id', channel);
